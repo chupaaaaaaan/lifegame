d395ac8a60d840133b1bc467d13be09cb2a1149a
chupaaaaaaan <jacobitebeerlove@yahoo.co.jp>
2019-01-04 09:34:10 +0000

    周期境界条件を実装

--- Main.hs
+++ Main.hs
@@ -14,12 +14,22 @@ class Functor w => Comonad w where
 
 data Z a = Z [a] a [a] deriving Show
 
+-- Z [] 1 [2,3,4,5]
+-- Z [1] 2 [3,4,5]
+-- Z [2,1] 3 [4,5]
+-- Z [3,2,1] 4 [5]
+-- Z [4,3,2,1] 5 []
 left :: Z a -> Z a
+left (Z [l] c rs) = let (b:a:ls) = reverse (l:c:rs)
+                    in Z ls a [b]
 left (Z (l:ls) c rs) = Z ls l (c:rs)
--- left (Z [] c rs) = Z [] c rs
+
+
+
 right :: Z a -> Z a
+right (Z ls c [r]) = let (a:b:rs) = reverse (r:c:ls)
+                     in Z [a] b rs
 right (Z ls c (r:rs)) = Z (c:ls) r rs
--- right (Z ls c []) = Z ls c []
 
 
 iterate1 :: (a -> a) -> a -> [a]
@@ -43,31 +53,6 @@ instance Comonad Z2 where
   extract (Z2 zz) = extract (extract zz)
   duplicate (Z2 zz) = fmap Z2 . Z2 . roll $ roll zz where
     roll z = Z (iterate1 (fmap left) z) z (iterate1 (fmap right) z)
-
--- :t zz
--- zz :: Z [Z [a] a [a]] (Z [a] a [a]) [Z [a] a [a]]
---    :: Z [Z a] (Z a) [Z a]
---    :: Z (Z a)
-
--- :t roll zz
--- roll zz :: Z [Z (Z a)] (Z (Z a)) [Z (Z a)]
---         :: Z (Z (Z a))
--- roll中のleft/rightは一番内側のZ aに作用する
-
--- :t roll $ roll zz
--- roll $ roll zz :: Z [Z (Z (Z a))] (Z (Z (Z a))) [Z (Z (Z a))]
---                :: Z (Z (Z (Z a)))
--- 外側のroll中のleft/rightは二番目の内側のZ (Z a)に作用する
-
--- :t Z2 . roll $ roll zz
--- Z2 . roll $ roll zz :: Z2 (Z (Z (Z (Z a))))
---                     :: Z2 (Z (Z a))
-
--- :t fmap Z2 . Z2 . roll $ roll zz
--- fmap Z2 . Z2 . roll $ roll zz :: fmap Z2 (Z2 (Z (Z a))
---                               :: Z2 (Z2 a)
-
-
 
 
 countNeighbours :: Z2 Bool -> Int
@@ -98,18 +83,20 @@ showZ2 w h (Z2 (Z _ _ rows)) = do
 
 
 
-
+-- Z2 $ Z [(Z "...." '.' "........."),(Z "...." '.' "........."),(Z "...." '.' "........."),...] (Z "...." '.' ".........") [(Z "...." '.' ".#......."),(Z "...." '.' "..#......"),(Z "...." '.' "###......"),(Z "...." '.' "........."),...]
 
 main :: IO ()
 main = do
-  let c2b c = if c == ' ' then False else True
-      b2c b = if b then '#' else ' '
+  let c2b c = if c == '.' then False else True
+      b2c b = if b then '#' else '.'
       (w, h) = (20, 20)
-      field = [ " # "
-              , "  #"
+      toZ' a xs = Z [a] a (take w $ xs ++ repeat a)
+      toZ2' a xss = Z2 $ toZ' (toZ' a []) (map (toZ' a) xss)
+      field = [ ".#."
+              , "..#"
               , "###"
               ]
-      initState = fmap c2b $ toZ2 ' ' field
+      initState = fmap c2b $ toZ2' '.' field
       loop state = do
         let state' = extend life state
         replicateM_ h $ putStr "\ESC[A\ESC[2K"
